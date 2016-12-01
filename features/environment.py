c38f37c45683203a15cd122b5756ec553f83704e
thinkmobilesqa <oleg.stasiv@thinkmobiles.com>
2016-12-01 17:02:14 +0200

    add Display

--- features/environment.py
+++ features/environment.py
@@ -1,9 +1,13 @@
 import os
-
+from pyvirtualdisplay import Display
 from selenium import webdriver
 
 def before_scenario(context, scenario):
+
+    display = Display(visible=1, size=(800, 800))
+    display.start()
     context.browser = webdriver.Chrome(executable_path='{}/chromedriver'.format(os.getcwd()))
+    #context.browser = webdriver.Chrome(executable_path='{}/chromedriver'.format(os.getcwd()))
     # context.browser = webdriver.Chrome() if you have set chromedriver in your PATH
     context.browser.set_page_load_timeout(10)
     context.browser.implicitly_wait(10)
